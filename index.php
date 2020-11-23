dda787290a536f678b3e28055aa6775e6724d97d
atiflovely999 <74484457+atiflovely999@users.noreply.github.com>
2020-11-23 17:36:36 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,132 @@
+﻿<html>
+<head>
+<title>
+my page
+</title>
+</head>
+<body bgcolor="000000">
+    <?php
+<h1 align="center"><font color="black red"><font size="10">بسم الله الرحمن الرحيم</h1></font>
+<br>
+<br>
+<img src="كتاب.jpg" width=300 beight=150>
+<img src="كتاب.jpg"  <h1 align="right"width=300 beight=150>
+<h1 align="center"><font color="red black"><font size="20">الكــــتـــــاب</h1></font>
+
+<p><h1 align="center"><font color="0000ff">هوالمعلم الاول للبشريه</h1></font></p>
+<br>
+<br>
+<h2 align="left">
+<a href="atif1992.html"><font color="ff5522">الصفحه االرئيسيه</a>
+<h1 align="center"><font color="ff5522">☝☝🤲🏼🤲🏼☝☝🤲🏼🤲🏼☝☝🤲🏼🤲🏼☝☝🤲🏼🤲🏼ــــــــــــــــــــــــــــ كتب اسلاميه  ـــــــــــــــــــــــــــ☝☝🤲🏼🤲🏼☝☝🤲🏼🤲🏼☝☝🤲🏼🤲🏼☝☝🤲🏼🤲🏼</h1></font>
+<br>
+<br>
+<h1 align="center"><font color="ff5522">قران الكريم بالانجليزي#</h3></font>
+<h1 align="center">
+<div style="padding:20px">
+<iframe type=application/pdf src ="1.pdf" width="700" height="700"></iframe>
+</div>
+
+<h1 align="center"><font color="ff5522">كتاب التوبه#</h3></font>
+<h1 align="center">
+<div style="padding:20px">
+<iframe type=application/pdf src ="2.pdf" width="700" height="700"></iframe>
+</div>
+
+<h1 align="center"><font color="ff5522">كتاب نصف دين#</h3></font>
+<h1 align="center">
+<div style="padding:20px">
+<iframe type=application/pdf src ="3.pdf" width="700" height="700"></iframe>
+</div>
+
+<h1 align="center"><font color="ff5522">كتاب خطوات نحو الملك#</h3></font>
+<h1 align="center">
+<div style="padding:20px">
+<iframe type=application/pdf src ="4.pdf" width="700" height="700"></iframe>
+</div>
+
+<h1 align="center"><font color="ff5522">كتاب فاتتني صلاه#</h3></font>
+<h1 align="center">
+<div style="padding:20px">
+<iframe type=application/pdf src ="5.pdf" width="700" height="700"></iframe>
+</div>
+
+<h1 align="center"><font color="ff5522">📙📙📙📙📙📙📙📙📙📙ــــــــــــــــــــــــــــ روايات  ـــــــــــــــــــــــــــ📙📙📙📙📙📙📙📙📙📙</h1></font>
+<h1 align="center"><font color="FF00FF">روايه شوق الدرويش#</h3></font>
+<h1 align="center">
+<div style="padding:20px">
+<iframe type=application/pdf src ="11.pdf" width="700" height="700"></iframe>
+</div>
+
+<h1 align="center"><font color="ff5522">روايه شيفره بلال#</h3></font>
+<h1 align="center">
+<div style="padding:20px">
+<iframe type=application/pdf src ="22.pdf" width="700" height="700"></iframe>
+</div>
+
+<h1 align="center"><font color="ff5522">روايه انت لي#</h3></font>
+<h1 align="center">
+<div style="padding:20px">
+<iframe type=application/pdf src ="33.pdf" width="700" height="700"></iframe>
+</div>
+
+<h1 align="center"><font color="ff5522">امنيتي ان اقتل رجلا#</h3></font>
+<h1 align="center">
+<div style="padding:20px">
+<iframe type=application/pdf src ="44.pdf" width="700" height="700"></iframe>
+</div>
+
+<h1 align="center"><font color="ff5522">ستعشقني رغما ان انفك#</h3></font>
+<h1 align="center">
+<div style="padding:20px">
+<iframe type=application/pdf src ="55.pdf" width="700" height="700"></iframe>
+
+
+
+
+
+
+
+
+
+
+
+$name="atif abaker";
+$class="IT 3";
+$subject="PHP";
+$addess="khartoum";
+$ID_student="123456";
+$Social_status="single";
+$Phone_number="0926999275";
+
+echo"my name is:"."$name"."<br>";
+echo"<br>";
+echo"class:"."$class"."<br>";
+echo"<br>";
+echo"subject:"."$subject"."<br>";
+echo"<br>";
+echo"addess:"."$addess"."<br>";
+echo"<br>";
+echo"ID_student:"."$ID_student"."<br>";
+echo"<br>";
+echo"Social_status:"."$Social_status"."<br>";
+echo"<br>";
+echo"Phone_number:"."$Phone_number"."<br>";
+
+?>
+
+
+
+
+
+
+
+
+
+
+
+
+
+</div>
+</body>
+</html>
